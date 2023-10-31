4e1eec42af6065f442b3b264d5d3251088f0f12d
jtirene <justintirene@gmail.com>
2023-10-31 00:55:05 -0400

    prettier

--- packages/functions/src/events/todo-created.ts
+++ packages/functions/src/events/todo-created.ts
@@ -1,6 +1,6 @@
-import { EventHandler } from "sst/node/event-bus";
-import { Todo } from "@task-manager/core/todo";
+import { Todo } from '@task-manager/core/todo'
+import { EventHandler } from 'sst/node/event-bus'
 
 export const handler = EventHandler(Todo.Events.Created, async (evt) => {
-  console.log("Todo created", evt);
-});
+	console.log('Todo created', evt)
+})
--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,14 @@
-import { SSTConfig } from "sst";
-import { API } from "./stacks/API";
+import { SSTConfig } from 'sst'
+import { API } from './stacks/API'
 
 export default {
-  config(_input) {
-    return {
-      name: "task-manager",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(API);
-  },
-} satisfies SSTConfig;
+	config(_input) {
+		return {
+			name: 'task-manager',
+			region: 'us-east-1',
+		}
+	},
+	stacks(app) {
+		app.stack(API)
+	},
+} satisfies SSTConfig
