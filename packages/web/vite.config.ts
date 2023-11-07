d9761c922ce2c7e5640eb0aea8637d8d9e601160
jtirene <justintirene@gmail.com>
2023-11-06 22:38:40 -0500

    shadcn ui

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,14 @@
 import generouted from '@generouted/react-router/plugin'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), generouted()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
