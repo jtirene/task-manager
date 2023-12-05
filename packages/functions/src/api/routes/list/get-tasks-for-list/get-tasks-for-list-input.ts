f512a8d8e075859fddcdfb0bbaa44cf28545f829
Justin Irene <justin@jtisoftware.com>
2023-12-05 18:51:20 -0500

    finished implementing tasks api

--- packages/functions/src/api/routes/list/get-tasks-for-list/get-tasks-for-list-input.ts
+++ packages/functions/src/api/routes/list/get-tasks-for-list/get-tasks-for-list-input.ts
@@ -1,3 +1,6 @@
 import { z } from 'zod'
+import { id } from '../../../../../../core/src/util/zod'
 
-export const GetTasksForListInput = z.object({})
+export const GetTasksForListInput = z.object({
+	listId: id,
+})
--- packages/functions/src/api/routes/list/get-tasks-for-list/get-tasks-for-list.ts
+++ packages/functions/src/api/routes/list/get-tasks-for-list/get-tasks-for-list.ts
@@ -1,6 +1,36 @@
+import { TRPCError } from '@trpc/server'
+import { eq } from 'drizzle-orm'
+import { Lists } from '../../../../../../core/src/entities/list/list.sql'
+import { db } from '../../../../../../core/src/util/db'
 import { UserProcedure } from '../../../procedure/user-procedure'
 import { GetTasksForListInput } from './get-tasks-for-list-input'
 
 export const GetTasksForList = UserProcedure.input(GetTasksForListInput).query(
-	async () => {},
+	async ({
+		ctx: {
+			user: { id },
+		},
+		input: { listId },
+	}) => {
+		const list = await db.query.Lists.findFirst({
+			where: eq(Lists.listId, listId),
+			with: {
+				tasks: true,
+			},
+		})
+
+		if (!list)
+			throw new TRPCError({
+				code: 'NOT_FOUND',
+				message: `List ${listId} not found`,
+			})
+
+		if (list.ownerId !== id)
+			throw new TRPCError({
+				code: 'UNAUTHORIZED',
+				message: `User ${id} is not authorized to get tasks for list ${listId}`,
+			})
+
+		return list.tasks
+	},
 )
